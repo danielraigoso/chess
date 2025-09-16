e1364dcf859586690c517636e749acaa7a25bc69
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-09-16 15:00:29 -0600

    passed all king move tests

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -55,36 +55,70 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         List<ChessMove> moves = new java.util.ArrayList<>();
 
-        //////////// BISHOP MOVES ////////////////////////////
-        int[][] dirs = { {+1,+1}, {-1,+1}, {-1,-1}, {+1,-1} };
-
         ChessPiece me = board.getPiece(myPosition);
         ChessGame.TeamColor myColor = me.getTeamColor();
 
-        for (int[] d : dirs) {
-            int r = myPosition.getRow();
-            int c = myPosition.getColumn();
+        /////////////////// BISHOP MOVES ////////////////////////////
 
-            while(true) {
-                r += d[0];
-                c += d[1];
-                if (r < 1 || r > 8 || c < 1 || c > 8) break;
+        if (getPieceType() == PieceType.BISHOP) {
+            // move diagonally
+            int[][] dirs = {{+1, +1}, {-1, +1}, {-1, -1}, {+1, -1}};
 
-                ChessPosition to = new ChessPosition(r,c);
-                ChessPiece target = board.getPiece(to);
+            // loop through every position
+            for (int[] d : dirs) {
+                int r = myPosition.getRow();
+                int c = myPosition.getColumn();
 
-                if (target == null) {
-                    // empty square keep moving
-                    moves.add(new ChessMove(myPosition, to, null));
-                } else {
-                    // piece found
-                    if (target.getTeamColor() != myColor) {
+                while (true) {
+                    r += d[0];
+                    c += d[1];
+                    // boundaries of board
+                    if (r < 1 || r > 8 || c < 1 || c > 8) break;
+
+                    ChessPosition to = new ChessPosition(r, c);
+                    ChessPiece target = board.getPiece(to);
+
+                    if (target == null) {
+                        // empty square keep moving
                         moves.add(new ChessMove(myPosition, to, null));
+                    } else {
+                        // piece found
+                        if (target.getTeamColor() != myColor) {
+                            moves.add(new ChessMove(myPosition, to, null));
+                        }
+                        break; //stop after any piece
                     }
-                    break; //stop after any piece
                 }
             }
+            return moves;
         }
-        return moves;
+
+        /////////////////// KING MOVES ////////////////////////////
+
+        if (getPieceType() == PieceType.KING) {
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+
+            int[][] kingMoves = {
+                    {-1, -1}, {-1,  0}, {-1, +1},  // SW, S, SE
+                    { 0, -1}, { 0, +1},            // W, E
+                    {+1, -1}, {+1,  0}, {+1, +1}   // NW, N, NE
+            };
+
+            for (int[] d : kingMoves) {
+                int r = row + d[0];
+                int c = col + d[1];
+                if (r < 1 || r > 8 || c < 1 || c > 8) continue;
+
+                ChessPosition to = new ChessPosition(r, c);
+                ChessPiece target = board.getPiece(to);
+
+                if (target == null || target.getTeamColor() != myColor) {
+                    moves.add(new ChessMove(myPosition, to, null));
+                }
+            }
+            return moves;
+        }
+        return List.of();
     }
 }
