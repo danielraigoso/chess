a5b3dbbead5ffed85da6d497c8e97e13a971b68e
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-10-20 19:59:25 -0600

    started ClearService

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,10 @@
+package service;
+
+import dataaccess.DataAccess;
+
+public class ClearService {
+    private final DataAccess db;
+    public ClearService(DataAccess db) {this.db = db;}
+
+    public void clear() {db.clearAll();}
+}
