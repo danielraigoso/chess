ced73d02f255dda324b2edaebc23775f9a3d364f
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-10-20 18:25:21 -0600

    phase 3: commit #16, working on GameDataDAO

--- server/src/main/java/dataaccess/GameDataDAO.java
+++ server/src/main/java/dataaccess/GameDataDAO.java
@@ -1,6 +1,49 @@
 package dataaccess;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import model.GameData;
 
-public class GameDataDAO {
+import javax.xml.crypto.Data;
+
+public class GameDataDAO implements GameDAO{
+    private final ConcurrentHashMap<Integer, GameData> games = new ConcurrentHashMap<>();
+    private final AtomicInteger nextId = new AtomicInteger(1);
+
+    @Override
+    public void clear(){
+        games.clear();
+        nextId.set(1);
+    }
+
+    @Override
+    public GameData create(String gameName) throws DataAccessException {
+        if (gameName == null || gameName.isBlank()) {
+            throw new DataAccessException("game name required");
+        }
+
+        int id = nextId.getAndIncrement();
+        var game = new GameData(id, null,null,gameName, null);
+        games.put(id,game);
+        return game;
+    }
+
+    @Override
+    public GameData find(int gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> list() {
+        return new ArrayList<>(games.values());
+    }
+
+    @Override
+    public void update(GameData game) throws DataAccessException {
+        if (game == null) {
+            throw new DataAccessException("null game");
+        }
+    }
 }
