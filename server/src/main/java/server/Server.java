1fa8cf7965757e931c4c5d094d992e55dc19bd01
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-10-20 19:39:43 -0600

    post /user javalin method implemented

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,15 +1,36 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import model.UserData;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessDAO;
+import org.eclipse.jetty.http.HttpTester;
+import service.ServiceException;
+import service.UserService;
 
 public class Server {
 
     private final Javalin javalin;
+    private final Gson gson = new Gson();
+    private final DataAccess db = new DataAccessDAO();
+    private final UserService userSvc = new UserService(db);
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        javalin.post("/user", ctx -> {
+            try {
+                var req = gson.fromJson(ctx.body(), UserData.class);
+                var auth = userSvc.register(req);
+                ctx.status(200).result(gson.toJson(new AuthOut(auth.username(), auth.authToken())));
+            } catch (ServiceException se) {
+                ctx.status(se.statusCode()).result(gson.toJson(new Message(se.getMessage())));
+            }
+        });
+
+
 
     }
 
@@ -21,4 +42,8 @@ public class Server {
     public void stop() {
         javalin.stop();
     }
+
+    private record Message(String message); {}
+    private record AuthOut(String username, String authToken) {}
+    private record Empty() {}
 }
