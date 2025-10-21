44459e4be12248ea582705fd0b0bcd004fb19f04
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-10-20 19:02:24 -0600

    phase 3: commit #21, attempt register user method

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -20,5 +20,25 @@ public class UserService {
         if (req == null || isBlank(req.username()) || isBlank(req.password()) || isBlank(req.email())) {
             throw new ServiceException(400, "Error: bad request");
         }
+
+        try {
+            if (db.users().find(req.username()) != null) {
+                throw new ServiceException(403, "Error: already taken");
+            }
+
+            db.users().insert(req);
+
+            var token = UUID.randomUUID().toString();
+            var auth = new AuthData(token, req.username());
+            db.auths().insert(auth);
+            return auth;
+
+            } catch (ServiceException se) {
+                throw se;
+            } catch (DataAccessException dae) {
+                throw new ServiceException(500, "Error: " + dae.getMessage());
+        }
     }
+
+    public AuthData
 }
