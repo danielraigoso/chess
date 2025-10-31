fa073711fb1ecf3626ba0f51200aa92a1c0808fd
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-10-31 09:33:45 -0600

    21 commits, starting to write tests

--- server/src/test/java/dataaccess/DataTests.java
+++ server/src/test/java/dataaccess/DataTests.java
@@ -1,4 +1,26 @@
 package dataaccess;
 
+import model.GameData;
+import org.junit.jupiter.api.*;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class DataTests {
+
+    private SqlGameDAO games;
+
+    @BeforeAll
+    static void bootstrapSchema() throws Exception {
+        DatabaseManager.createDatabase();
+
+        SqlUserDAO.createTable();
+        SqlAuthDAO.createTable();
+        SqlGameDAO.createTable();
+    }
+
+    
 }
+
