c339d52052cf475babce4eec96547663f0c4a47f
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-10-31 10:03:24 -0600

    22 commits, writing pass fail tests

--- server/src/test/java/dataaccess/DataTests.java
+++ server/src/test/java/dataaccess/DataTests.java
@@ -21,6 +21,49 @@ public class DataTests {
         SqlGameDAO.createTable();
     }
 
-    
+    @BeforeEach
+    void setUp() throws Exception {
+        games = new SqlGameDAO();
+        try {
+            new SqlAuthDAO().clear();
+            new SqlGameDAO().clear();
+            new SqlUserDAO().clear();
+        } catch (DataAccessException dae) {
+            throw dae;
+        }
+    }
+
+    @Test
+    void clear_success() throws Exception {
+        // seed one game
+        var g = games.create("G1");
+        assertNotNull(g);
+        // clear
+        games.clear();
+        Collection<GameData> all = games.list();
+        assertTrue(all.isEmpty(), "clear() should remove all rows");
+    }
+
+    //create(String) pass/fail
+    @Test
+    void create_success() throws Exception {
+        var g = games.create("MyGame");
+        assertNotNull(g);
+        assertTrue(g.gameID() > 0);
+        assertEquals("MyGame", g.gameName());
+        assertNull(g.whiteUsername());
+        assertNull(g.blackUsername());
+        assertNull(g.game());
+    }
+
+    @Test
+    void create_fail() {
+        assertThrows(DataAccessException.class, () -> games.create(""));
+    }
+
+    @Test
+    void create_fail_nullName() {
+        assertThrows(DataAccessException.class, () -> games.create(null));
+    }
 }
 
