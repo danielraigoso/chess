93696533dac0c7ecc17895d23db8946b42a20bd2
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-10-30 21:41:11 -0600

    10 commit changed game interface

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -4,9 +4,9 @@ import java.util.Collection;
 import model.GameData;
 
 public interface GameDAO {
-    void clear();
+    void clear() throws DataAccessException;
     GameData create(String gameName) throws DataAccessException;
-    GameData find(int gameID);
-    Collection<GameData> list();
+    GameData find(int gameID) throws DataAccessException;
+    Collection<GameData> list() throws DataAccessException;
     void update(GameData game) throws DataAccessException;
 }
