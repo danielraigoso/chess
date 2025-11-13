2bdfffed9d7354d2724704092d5ce81008a730d0
Daniel Nielsen <daniel.nielsen621@gmail.com>
2025-11-13 15:49:19 -0700

    commit #7 wrote ServerFacade register

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,14 +1,41 @@
 package client;
 
+import model.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import model.AuthData;
-import model.GameData;
-import 
+
+
 public class ServerFacade {
+    private final String baseUrl;
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final Gson gson = new Gson();
+
+    public ServerFacade(int port) {
+        this.baseUrl = "http://localhost:" + port;
+    }
+
+    public AuthData register(String username, String password, String email) throws IOException, InterruptedException {
+        var user = new UserData(username, password, email);
+        var body = gson.toJson(user);
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + "/user"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new RuntimeException(response.body());
+        }
+
+        return gson.fromJson(response.body(), AuthData.class);
+    }
+
+
 }
